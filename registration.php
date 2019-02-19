7a6ce1e8beb8fc4ca122822c1acdaa8a0522be21
Lucyjiy <45378571+Lucyjiy@users.noreply.github.com>
2019-02-19 13:32:11 +0800

    Add files via upload

---
+++ registration.php
@@ -0,0 +1,15 @@
+<html>
+<body>
+
+<form action="welcome.php" method="post">
+Name: <input type="text" name="name"><br>
+E-mail: <input type="text" name="email"><br>
+Password: <input type="text" name="Password"><br>
+Confirm password: <input type="text" name="Confirm Password"><br>
+
+
+<input type="submit">
+</form>
+
+</body>
+</html>
---
+++ welcome.php
@@ -0,0 +1,9 @@
+<html>
+<body>
+
+Welcome <?php echo $_POST["name"]; ?><br>
+You are now Registered!!
+
+
+</body>
+</html>
